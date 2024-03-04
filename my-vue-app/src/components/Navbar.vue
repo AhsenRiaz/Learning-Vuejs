019241ad87509748f7d487bd70e09fd1cfbf8439
Ahsen Riaz <ahsenriaz2001@gmail.com>
2024-03-04 10:17:03 +0500

    feat: Navbar component

---
+++ my-vue-app/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+    data() {
+        return {
+            activePage: 0,
+            pages: [
+                {
+                    link: { text: 'Home', url: 'index.html' },
+                    pageTitle: 'Home Page',
+                    content: 'This is the home content'
+                },
+                {
+                    link: { text: 'About', url: 'about.html' },
+                    pageTitle: 'About Page',
+                    content: 'This is the about content'
+                },
+                {
+                    link: { text: 'Contact', url: 'contact.html' },
+                    pageTitle: 'Contact Page',
+                    content: 'This is the contact content'
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li v-for="(page, index) in pages" class="nav-item">
+                        <a class="nav-link active" @click.prevent = "activePage = index" aria-current="page" :href="page.link.url">{{ page.link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="content" >
+        <h1>{{ pages[activePage].pageTitle }}</h1>
+    </div>
+
+</template>
+
+<style scoped></style>
